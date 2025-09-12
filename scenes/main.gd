9860d9bd9d6a96a81972f8d51392a256dc9f1d3c
Henry Chao <henchao@gmail.com>
2025-09-11 20:43:43 -0400

    Adjust lights and baby toy detection.

--- scenes/main.gd
+++ scenes/main.gd
@@ -10,12 +10,23 @@ func _ready() -> void:
 
 
 func _on_player_menu_button_hit() -> void:
+	if get_tree().paused:
+		_unpause_game()
+	else:
+		_pause_game()
+
+
+func _on_paused_menu_resume_game_button_pressed() -> void:
+	_unpause_game()
+
+
+func _pause_game() -> void:
 	get_tree().paused = true
 	paused_menu.visible = true
 	game_objects.visible = false
 
 
-func _on_paused_menu_resume_game_button_pressed() -> void:
+func _unpause_game() -> void:
 	paused_menu.visible = false
 	game_objects.visible = true
 	get_tree().paused = false
